b2880d6466ba163837162aaa56ea4581f76e22b0
iprince <imprince0712@gmail.com>
2025-03-01 17:29:10 +1030

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Git_Trial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
